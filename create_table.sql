aa0a7bb9b0b8017cb9104ebb1127ba1a2c099293
arnazalavi <arnazalavi@gmail.com>
2021-09-19 16:23:15 -0500

    new version of sql

--- create_table.sql
+++ create_table.sql
@@ -1,55 +1,61 @@
 
+Step- 1
+
+Create a new Database UNICEF_Data_Warehouse 
+
+Step 2.
+Create tables to load the data from the 2 different csv files:
+
+-drop table child_mortality
+
 CREATE TABLE child_mortality (
-country_code_name	  VARCHAR(255) ,
+country_code_name	  VARCHAR(255) not null ,
 indicator	  VARCHAR(255)   ,
-sex     VARCHAR(255) ,
-timeperiod 	VARCHAR(25)   ,
+sex     VARCHAR(255) not null ,
+timeperiod 	VARCHAR(25)  not null ,
+observation		VARCHAR(255)   ,
+unit_of_measure VARCHAR(255),
+PRIMARY KEY (country_code_name,sex,timePeriod )
+);
+
+select * from  child_mortality M
+
+drop table child_poverty
+
+CREATE TABLE child_poverty (
+country_code_name	  VARCHAR(255) not null  ,
+indicator	  VARCHAR(255)   ,
+sex     VARCHAR(255) not null ,
+timeperiod 	VARCHAR(25) not null   ,
 observation		VARCHAR(255)   ,
 unit_of_measure VARCHAR(255),
 PRIMARY KEY (country_code_name,sex,timePeriod )
 );
 
 
-
-CREATE TABLE child_poverty (
-country_code_name	  VARCHAR(255) ,
-indicator	  VARCHAR(255)   ,
-sex     VARCHAR(255) ,
-timeperiod 	VARCHAR(25)   ,
-observation		VARCHAR(255)   ,
-unit_of_measure VARCHAR(255)
-);
+---Query 
+select * from  child_mortality 
+where country_code_name = 'Afghanistan'
+and timeperiod = '2009'
+and sex = 'Female'
 
 
 
-select * from  child_mortality
-where "CountryCodeName" like 'AFG%'
-and "TimePeriod" = '2009'
-and "Sex" like 'F%'
-
-CREATE TABLE CHILD_POVERTY (
-Country_Code_Name	  VARCHAR(255) NOT NULL  PRIMARY KEY,
-Indicator	  VARCHAR(255) NOT NULL  ,
-Sex      VARCHAR(255) NOT NULL  ,
-TimePeriod 	VARCHAR(25)   ,
-Observation	 		VARCHAR(255)   ,
-Unit_of_measure VARCHAR(255)
-);
+select M.country_code_name ,M.observation as mortality_observation , M.unit_of_measure as mortality_unit_of_measure,
+P.observation as poverty_observation , P.unit_of_measure as poverty_unit_of_measure,
+M.timeperiod,
+M.sex
+from child_mortality M
+Join child_poverty P
+on M.country_code_name = P.country_code_name
+and M.timeperiod = p.timeperiod
+and M.sex=p.sex
 
 
-Query
+select * from child_poverty
 
 select * from  child_mortality M
-join CHILD_POVERTY P
-on M."CountryCodeName" = P."CountryCodeName"
-and M."Sex" = P."Sex" 
-and M."TimePeriod" = P."TimePeriod"
-
-select * from  child_mortality
-where "CountryCodeName" like 'AFG%'
-and "TimePeriod" = '2009'
-and "Sex" like 'F%'
 
 
-new_UNICEF_child_poverty_data_df = new_UNICEF_child_poverty_data_df.rename(columns={'REF_AREA:Geographic area':'CountryCodeName','INDICATOR:Indicator':'Indicator',
-'OBS_VALUE:Observation Value':'Observation','UNIT_MULTIPLIER:Unit multiplier':'Unit_of_measure','SEX:Sex':'Sex','TIME_PERIOD:Time period':'TimePeriod'} ,inplace = False)
+
+
